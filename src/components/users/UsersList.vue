9b1663cec1ab61c43ffbf7c2ff22952875b380f4
YourName <noor11809027@gmail.com>
2022-09-16 16:28:16 +0600

    programatic route

--- src/components/users/UsersList.vue
+++ src/components/users/UsersList.vue
@@ -1,7 +1,10 @@
 <template>
+
   <ul>
     <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
+    <button @click="confirm">Confirm</button>
   </ul>
+ 
 </template>
 
 <script>
@@ -12,6 +15,11 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  methods:{
+    confirm(){
+      this.$router.push('/teams');
+    }
+  }
 };
 </script>
 
@@ -21,5 +29,15 @@ ul {
   margin: 2rem auto;
   max-width: 20rem;
   padding: 0;
+}
+button{
+  background-color: aquamarine;
+  border: 0px;
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 10px 10px 10px rgb(90, 26, 26);
+}
+button:hover{
+  background-color: rgba(14, 80, 18, 0.521);
 }
 </style>
