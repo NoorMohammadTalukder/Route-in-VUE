16c8b12af33821ee9d71454f424f6423a809b5d3
YourName <noor11809027@gmail.com>
2022-09-16 15:09:06 +0600

    route URL add

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import { createApp } from 'vue';
+import { createRouter,createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+// import TeamList from './components/teams/TeamList.vue';
+import UserList from './components/users/UsersList.vue'
 
+import TeamList from './components/teams/TeamsList.vue'
+
+const router=createRouter({
+    history:createWebHistory(),
+    routes:[
+        {
+            path:'/teams',component:TeamList
+        },
+        {
+            path:'/users',component:UserList
+        },
+    ], 
+});
 const app = createApp(App)
+
+app.use(router);
 
 app.mount('#app');
